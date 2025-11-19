05ccf269dd3222d686981a7922d0b2603e80c29a
KAMARJAHAN <ztenkammu@gmail.com>
2025-11-19 21:40:26 +0530

    Update IconLink.jsx

--- components/IconLink.jsx
+++ components/IconLink.jsx
@@ -1,1 +1,6 @@
-
+import React from 'react';
+export default function IconLink({ href, children }) {
+  return (
+    <a href={href} target="_blank" rel="noreferrer" className="inline-flex items-center gap-2 hover:underline">{children}</a>
+  );
+}
